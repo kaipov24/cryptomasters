1b14882ef58a414466aed8e9017a35ea76aa4ce5
kaipov24 <kaipovich24@gmail.com>
2025-06-03 16:14:36 +0600

    Loop fetching

--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/kaipov24/cryptomsters/api"
 )
 
 func main() {
-	rate, err := api.GetRate("BTC")
+	currencies := []string{"BTC", "ETH"}
+	var wg sync.WaitGroup
+	for _, currency := range currencies {
+		wg.Add(1)
+		go func(currencyCode string) {
+			getCurrencyData(currencyCode)
+			wg.Done()
+		}(currency)
+	}
+	wg.Wait()
+}
+
+func getCurrencyData(currency string) {
+	rate, err := api.GetRate(currency)
 	if err == nil {
 		fmt.Printf("The current price of %v is %.8f \n", rate.Currency, rate.Price)
 	}
-	fmt.Println(rate.Price, err)
 }
